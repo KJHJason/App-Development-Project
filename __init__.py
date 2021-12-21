d0968066d448ad81d0496d2fd8c7ce6c7b7a61e0
KJHJason <kuanjunhaojason@gmail.com>
2021-12-21 23:21:35 +0800

    Corrections to the init.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, abort
+from flask import Flask, render_template, request, redirect, url_for
 from wtforms import StringField, PasswordField
 from werkzeug.utils import secure_filename
 import os
