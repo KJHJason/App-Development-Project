8ad40c153424a0eaafafe49c92586053084a0857
KJHJason <kuanjunhaojason@gmail.com>
2021-12-21 23:20:07 +0800

    Moved some files and coded the user profile settings wireframe

--- __init__.py
+++ __init__.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort
+from wtforms import StringField, PasswordField
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+UPLOAD_PATH = '/static/images/user'
+ALLOWED_EXTENSIONS = {"png"}
+app.config['UPLOAD_PATH'] = "uploads"
+app.config['MAX_CONTENT_LENGTH'] = 5120 * 5120
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/user_profile')
+def userProfile():
+    return render_template('user_profile.html')
+    
 
 if __name__ == '__main__':
     app.run()
