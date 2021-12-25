01ac68b2217212052e7da747626cbe1e43dafd31
KJHJason <kuanjunhaojason@gmail.com>
2021-12-26 01:33:37 +0800

    changed the hamburger icon for a better looking one

--- static/scripts/common.js
+++ static/scripts/common.js
@@ -67,3 +67,14 @@ document.addEventListener("DOMContentLoaded", function(){
   })()
 
 ////////////////// End of Boostrap 5 Tooltip Javascript //////////////////
+
+//////////////////START OF HAMBURGER MENU JAVASCRIPT //////////////////
+
+let navbtn = document.getElementById("nav-icon");
+
+function btnclick(){
+    navbtn.classList.toggle("openTrue"); // it will toggle the class "open", i.e. add class="open" or remove class="open" to the hamburger icon which has the id, nav-icon
+}
+
+navbtn.addEventListener("click", btnclick);
+////////////////// END OF HAMBURGER MENU JAVASCRIPT //////////////////
