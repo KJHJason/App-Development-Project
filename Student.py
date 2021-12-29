2394ee058d6a33856c174b615b483825f831cdb6
MikuChanUwU <57163606+MikuChanUwU@users.noreply.github.com>
2021-12-29 22:05:39 +0800

    fixed some issues

--- Student.py
+++ Student.py
@@ -8,6 +8,10 @@ class Student(User):
         self.__card_expiry = ""
         self.__card_cvv = ""
         self.__card_type = ""
+        self.__purchaseID = ""
+        self.__reviewID = ""
+        self.__viewed = ""
+        
 
     def set_card_name(self, card_name):
         self.__card_name = card_name
@@ -19,6 +23,12 @@ class Student(User):
         self.__card_cvv = card_cvv
     def set_card_type(self, card_type):
         self.__card_type = card_type
+    def set_purchaseID(self, purchaseID):
+        self.__purchaseID = purchaseID
+    def set_reviewID(self, reviewID):
+        self.__reviewID = reviewID
+    def set_viewed(self, viewed):
+        self.__viewed = viewed
 
     def get_card_name(self):
         return self.__card_name
@@ -30,6 +40,12 @@ class Student(User):
         return self.__card_cvv
     def get_card_type(self):
         return self.__card_type
+    def get_purchaseID(self):
+        return self.__purchaseID
+    def get_reviewID(self):
+        return self.__reviewID
+    def get_viewed(self):
+        return self.__viewed
 
     def display_card_info(self):
         print("student's name:", self.get_username(), "card name:", self.__card_name, "card number:", self.__card_no, "card expiry:", self.__card_expiry, "card cvv:", self.__card_cvv, "card type:", self.__card_type)
