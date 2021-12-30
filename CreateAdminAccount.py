4bf87c2fbdc06726a717d0998e4baa00acffe3ad
KJHJason <kuanjunhaojason@gmail.com>
2021-12-30 20:02:47 +0800

    Edited Security.py and added extra validation to CreateAdminAccount.py

--- CreateAdminAccount.py
+++ CreateAdminAccount.py
@@ -12,10 +12,10 @@ def validate_email(email):
 
 def validate_pwd_length(pwd, pwdMinimumLength):
     if len(pwd) < pwdMinimumLength:
-        print("Please enter a password with at least {} characters!" .format(pwdMinimumLength))
+        print("\nError: Please enter a password with at least {} characters!" .format(pwdMinimumLength))
         return False
     else:
-        print("Password lenght accepted, within {} characters minimum requirement." .format(pwdMinimumLength))
+        print("Verdict: Password length accepted, within {} characters minimum requirement." .format(pwdMinimumLength))
         return True
 
 # regular expression for validating an Email
@@ -40,33 +40,37 @@ while True:
     if cmd_input == "1":
         username = Sanitise(input("Enter username for admin account: "))
         email = Sanitise(input("Enter email for admin account: "))
-        print("Validating Email...")
-        emailValid = validate_email(email)
-        if emailValid:
-            password = input("Enter password for admin account: ")
+        emailValid = False
+        if email != False:
+            print("\nValidating Email...")
+            emailValid = validate_email(email)
+        if emailValid and username != False and email != False:
+            password = input("\nEnter password for admin account: ")
             
             pwdLengthValidate = validate_pwd_length(password, 6) # change the number accordingly to specify the minimum characters of the password that is required (accordingly to CourseFinity's password policy)
 
             if pwdLengthValidate:
-                password = pwdManager.admin_hash_password(password)
-                cfm_password = input("Confirm password for admin account: ")
+                password = pwdManager.hash_password(password)
+                cfm_password = input("\nConfirm password for admin account: ")
 
-                pwdMatched = pwdManager.admin_verify_password(password, cfm_password)
+                pwdMatched = pwdManager.verify_password(password, cfm_password)
 
                 if pwdMatched:
-                    print("Password for admin entered matched.")
+                    print("\nPassword for admin entered matched.")
                     # Retrieving data from shelve for duplicate data checking
                     adminDict = {}
                     db = shelve.open("admin", "c")  # "c" flag as to create the files if there were no files to retrieve from and also to create the user if the validation conditions are met
                     try:
                         if 'Admins' in db:
                             adminDict = db['Admins']
+                            print("\nRetrieved data from admin account database")
                         else:
-                            print("No admin account data in user shelve files")
+                            print("\nError: No admin account data in admin account database")
                             db["Admins"] = adminDict
+                            print("Error resolved: Created an empty admin account database")
                     except:
                         db.close()
-                        print("Error in retrieving Admins from user.db")
+                        print("\nError in retrieving Admins from user.db")
                     
                     email_duplicates = False
                     username_duplicates = False
@@ -74,7 +78,7 @@ while True:
                     adminIDShelveData = 0 # initialise to 0 as the shelve files can be missing or new which will have no data
                     # checking duplicates for username and getting the latest possible user ID from the admin shelve files
                     for key in adminDict:
-                        print("retrieving usernames")
+                        print("\nretrieving usernames")
                         usernameShelveData = adminDict[key].get_username()
                         adminIDShelveData = int(adminDict[key].get_user_id())
                         adminIDShelveData += 1 # add 1 to get the next possible user ID if there is/are user data in the user shelve files
@@ -85,7 +89,7 @@ while True:
 
                     # Checking duplicates for email
                     for key in adminDict:
-                        print("retrieving emails")
+                        print("\nretrieving emails")
                         emailShelveData = adminDict[key].get_email()
                         if email == emailShelveData:
                             print("Error: Admin email already exists.")
@@ -93,28 +97,33 @@ while True:
                             break
                     
                     if email_duplicates and username_duplicates:
-                        print("Duplicate admin username and password, please enter unique username and password!")
+                        print("\nError: Duplicate admin username and password, please enter unique username and password!")
                         db.close()
                     else:
                         if email_duplicates:
                             db.close()
-                            print("Duplicate admin email, please enter a unique email.")
+                            print("\nError: Duplicate admin email, please enter a unique email.")
                         else:
                             if username_duplicates:
                                 db.close()
-                                print("Duplicate admin username, please enter a unique username.")
+                                print("\nError: Duplicate admin username, please enter a unique username.")
                             else:
                                 admin = Admin(adminIDShelveData, username, email, password)
                                 adminDict[adminIDShelveData] = admin
-                                print("Admin account created.")
+                                print("\nAdmin account created/updated in the admin account database")
                                 db["Admins"] = adminDict
                                 db.close()
                                 print("Admin account ID:", admin.get_user_id())
                 else:
-                    print("Password for admin entered did not matched.")
+                    print("\nError: Password for admin entered did not matched.")
+        else:
+            if email == False:
+                print("\nError: You cannot enter an empty input for the email!")
+            if username == False:
+                print("\nError: You cannot enter an empty input for the username!")
 
     elif cmd_input == "0":
-        print("Thank you and have a nice day.")
+        print("\nThank you and have a nice day.")
         quit()
     else:
-        print("Invalid command, please enter the number 1 or 0 as specified in the control panel guide.")
+        print("\nError: Invalid command, please enter the number 1 or 0 as specified in the control panel guide.")
--- Security.py
+++ Security.py
@@ -3,7 +3,7 @@ import html
 
 """Password hashing"""
 
-# things to note, argon2 by default will use 65536KB of memory and time is 3 iterations, and 4 degrees of parallelism by default
+# things to note, argon2 by default will generate a random salt and use 65536KB of memory and time is 3 iterations, and 4 degrees of parallelism when hashing
 
 # minimum requirement as of OWASP; Use Argon2id with a minimum configuration of 15 MiB of memory (15728KB), an iteration count of 2, and 1 degree of parallelism.
 # OWASP website: https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html
@@ -12,45 +12,17 @@ class PasswordManager:
     def __init__(self):
         self.hasher = PasswordHasher()
 
-    def salt_password(self, pwd):
-        # salting the password for extra security, do not change the salt as all the users' password must have the same salt
-        self.__salt = "9#ZcH%"
-        self.__salted_pwd = pwd + self.__salt
-        return self.__salted_pwd
-
     def hash_password(self, pwd):
-        self.__pwd = self.salt_password(pwd)
-        return self.hasher.hash(self.__pwd)
+        return self.hasher.hash(pwd)
 
     def verify_password(self, hashed, pwd):
         # try and except as argon2 will raise an exception if the hashes are not matched
         try:
-            self.__pwd = self.salt_password(pwd)
-            return self.hasher.verify(hashed, self.__pwd)
+            return self.hasher.verify(hashed, pwd) # will return True if both the hash matches
         except:
             return False
 
-    # different salt for the admin account for extra security as if the user's salt somehow got cracked, the attackers will still have to crack the admin's salt
-    def admin_salt_password(self, pwd):
-        # salting the password for extra security, do not change the salt as all the admins' password must have the same salt
-        self.__admin_salt = "C%qf9D"
-        self.__admin_salted_pwd = pwd + self.__admin_salt
-        return self.__admin_salted_pwd
-
-    def admin_hash_password(self, pwd):
-        self.__admin_pwd = self.admin_salt_password(pwd)
-        return self.hasher.hash(self.__admin_pwd)
-
-    def admin_verify_password(self, hashed, pwd):
-        # try and except as argon2 will raise an exception if the hashes are not matched
-        try:
-            self.__admin_pwd = self.admin_salt_password(pwd)
-            return self.hasher.verify(hashed, self.__admin_pwd)
-        except:
-            return False
-
-""" 
-# for testing purposes
+""" # for testing purposes
 pwdManager = PasswordManager()
 storing_hashed = pwdManager.hash_password("test123")
 print(storing_hashed)
