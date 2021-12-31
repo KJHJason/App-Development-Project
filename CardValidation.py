b312a5b185441dc5da979618507bd31c886289cf
KJHJason <kuanjunhaojason@gmail.com>
2022-01-01 02:52:43 +0800

    Reorganised some blocks of codes and added comments

--- CardValidation.py
+++ CardValidation.py
@@ -19,6 +19,49 @@ import re
 def intList(numbers):
     return list(map(int, str(numbers)))
 
+# function to recognise a card type by the card number
+def get_card_type(cardNumber):
+    try:
+        cardNoList = intList(cardNumber)
+        firstDigit = cardNoList[0] # getting the first digit of the credit card number
+        if firstDigit == 4:
+            # Visa cards starts with the number 4
+            return "visa"
+        elif firstDigit == 5 or firstDigit == 2:
+            # MasterCard 5-series starts with the number 5 and MasterCard 2-series starts with the number 2
+            return "mastercard"
+        else:
+            return False
+    except:
+        print("Card number input must only contain numbers!")
+        return False
+
+# function to validate card's CVV based on the card type
+def validate_cvv(cardCVV, cardType):
+    # try and except as in the __init__.py, I validated the sanitise the CVV so if it return False for some reason (empty strings, etc.), it will go to the except block and return False
+    if cardType == "visa" or cardType == "mastercard":
+        try:
+            # for 3 digits CVV cards
+            regex3D = re.compile(r"^[0-9]{3}$") # compile the regex so that it does not have to rewrite the regex
+
+            if(re.match(regex3D, cardCVV)):
+                return True
+            else:
+                return False
+        except:
+            return False # if the cardCVV variable contained a string with letters, it will return False
+    else:
+        # for 4 digits CVV cards
+        try:
+            regex4D = re.compile(r"^[0-9]{4}$") # compile the regex so that it does not have to rewrite the regex
+
+            if(re.match(regex4D, cardCVV)):
+                return True
+            else:
+                return False
+        except:
+            return False # if the cardCVV variable contained a string with letters, it will return False
+
 # main function to validate credit cards using the Luhn's algorithm, aka the modulus 10 or mod 10 algorithm
 def validate_card(cardNumber, cardCVV):
     try:
@@ -59,44 +102,3 @@ def validate_card(cardNumber, cardCVV):
     except:
         print("Card number input must only contain numbers!") # if the string contained any letters, it will raise a runtime error. Hence, using try and except to handle this error.
         return False
-
-def get_card_type(cardNumber):
-    try:
-        cardNoList = intList(cardNumber)
-        firstDigit = cardNoList[0] # getting the first digit of the credit card number
-        if firstDigit == 4:
-            # Visa cards starts with the number 4
-            return "visa"
-        elif firstDigit == 5 or firstDigit == 2:
-            # MasterCard 5-series starts with the number 5 and MasterCard 2-series starts with the number 2
-            return "mastercard"
-        else:
-            return False
-    except:
-        print("Card number input must only contain numbers!")
-        return False
-
-def validate_cvv(cardCVV, cardType):
-    # try and except as in the __init__.py, I validated the sanitise the CVV so if it return False for some reason (empty strings, etc.), it will go to the except block and return False
-    if cardType == "visa" or cardType == "mastercard":
-        try:
-            # for 3 digits CVV cards
-            regex3D = re.compile(r"^[0-9]{3}$") # compile the regex so that it does not have to rewrite the regex
-
-            if(re.match(regex3D, cardCVV)):
-                return True
-            else:
-                return False
-        except:
-            return False # if the cardCVV variable contained a string with letters, it will return False
-    else:
-        # for 4 digits CVV cards
-        try:
-            regex4D = re.compile(r"^[0-9]{4}$") # compile the regex so that it does not have to rewrite the regex
-
-            if(re.match(regex4D, cardCVV)):
-                return True
-            else:
-                return False
-        except:
-            return False # if the cardCVV variable contained a string with letters, it will return False
