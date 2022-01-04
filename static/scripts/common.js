e17461d6a90c7d5172ba8b9bd79b3b8c1eea1e69
KJHJason <kuanjunhaojason@gmail.com>
2022-01-04 18:13:10 +0800

    Changed labelling for card number input (to improve UXD)

--- static/scripts/common.js
+++ static/scripts/common.js
@@ -1,12 +1,9 @@
 ////////////////// Boostrap 5 Tooltip Javascript //////////////////
 
-(function () {
-    'use strict'
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
-      new bootstrap.Tooltip(tooltipTriggerEl)
-    })
-  })()
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
 
 ////////////////// End of Boostrap 5 Tooltip Javascript //////////////////
 
