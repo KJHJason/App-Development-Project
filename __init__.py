05b08cdaccfbd2184e1958a0f37eb5a9e6adcc9f
MikuChanUwU <57163606+MikuChanUwU@users.noreply.github.com>
2022-01-06 22:53:15 +0800

    edit on init

--- __init__.py
+++ __init__.py
@@ -2013,19 +2013,48 @@ def purchaseHistory():
             purchaseID = userKey.get_purchaseID()
             print("PurchaseID exists?: ", purchaseID)
 
-            try:
-                if purchaseID:
-                    pass
+            #try:
+                #if purchaseID:
+                    #pass
 
-                else:
-                    pass
-            except:
-                db.close()
-                print("Error in displaying Purchase History")
-                return redirect(url_for("home"))
+                #else:
+                    #pass
+            #except:
+                #db.close()
+                #print("Error in displaying Purchase History")
+                #return redirect(url_for("home"))
+
+            courseDict = {
+                0: {'name': 'Python 101', 'description': "lorem ipsum on deez python nuts", 'price': 420, "tag": "programming"},
+                1: {'name': 'Maths 101', 'description': "lorem ipsum on deez maths nuts", 'price': 69, "tag": "maths"},
+                2: {'name': 'Science 101', 'description': "lorem ipsum on deez science nuts", 'price': 1337, "tag": "science"}
+            }
+
+
+
+            print(courseDict[1]['name'])
+
+            coursePurchasedList = []
+            purchaseHistoryDict = {
+                0:courseDict[2]
+            }
+
+            for purchasedCourseID in purchaseHistoryDict:
+                coursePurchasedList.append(purchasedCourseID)
+
+            print(purchaseHistoryDict[0]) 
+            
+            # pagination algorithm starts here
+            purchaseHistoryList = purchaseHistoryList[::-1] # reversing the list to show the newest users in CourseFinity using list slicing
+            pageNumForPagination = pageNum - 1 # minus for the paginate function
+            paginatedUserList = paginate(purchaseHistoryList, pageNumForPagination, maxItemsPerPage)
+            paginationList = get_pagination_button_list(pageNum, maxPages)
+            
+            previousPage = pageNum - 1
+            nextPage = pageNum + 1
 
             db.close() # remember to close your shelve files!
-            return render_template('users/loggedin/purchasehistory.html')
+            return render_template('users/admin/user_management.html', userList=paginatedUserList, count=userListLen, maxPages=maxPages, pageNum=pageNum, paginationList=paginationList, nextPage=nextPage, previousPage=previousPage)
         else:
             db.close()
             print("User not found or is banned")
