d234780c30f569481bd4cfd72f72ca0020eec373
KJHJason <kuanjunhaojason@gmail.com>
2022-01-07 22:46:22 +0800

    added jsonify in preparation for PayPal

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from werkzeug.utils import secure_filename
 import shelve, os, math, paypalrestsdk
 import Student, Teacher, Admin, Forms
