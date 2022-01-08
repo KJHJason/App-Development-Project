605ea8e23b7e634038a12a6406dc85d01f491f9d
KJHJason <kuanjunhaojason@gmail.com>
2022-01-09 01:43:15 +0800

    Improved UXD for admin user management feature

--- CreateXNumOfUsers.py
+++ CreateXNumOfUsers.py
@@ -1,7 +1,15 @@
 import shelve
 import Student
 from Security import hash_password
-from IntegratedFunctions import get_userID
+
+def get_userID(userDict):
+    userIDShelveData = 0 # initialise to 0 as the shelve files can be missing or new which will have no data
+    for key in userDict:
+        print("retrieving")
+        userIDShelveData = int(userDict[key].get_user_id())
+        print("ID in database:", userIDShelveData)
+        userIDShelveData += 1 # add 1 to get the next possible user ID if there is/are user data in the user shelve files
+    return userIDShelveData
 
 userDict = {}
 db = shelve.open("user", "c")
