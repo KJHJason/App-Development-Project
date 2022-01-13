f2bb39883b8b0132719cdaf9a905fd50fd601407
KJHJason <kuanjunhaojason@gmail.com>
2022-01-13 20:27:12 +0800

    Fixed teacher bio frontend flaw

--- static/scripts/user_profile.js
+++ static/scripts/user_profile.js
@@ -1,12 +1,14 @@
+// to set a cookie to contain the file size value upon submission
+function filesize(element) {
+    document.cookie = `filesize = ${element.files[0].size}`; // using string interpolation to get the value of the image file size and element.files[0] for the first file's size
+}
+
+// for editing teacher's bio
 function editBioNow() {
-    document.getElementById("editBio").setAttribute("hidden", null);
-    document.getElementById("saveBio").removeAttribute("hidden");
-    document.getElementById("cancleSaveBio").removeAttribute("hidden");
-    document.getElementById("teacherBio").removeAttribute("disabled");
+    document.getElementById("originalTextarea").setAttribute("hidden", null);
+    document.getElementById("editedTextarea").removeAttribute("hidden");
 }
 function cancelBio() {
-    document.getElementById("saveBio").setAttribute("hidden", null);
-    document.getElementById("cancleSaveBio").setAttribute("hidden", null);
-    document.getElementById("editBio").removeAttribute("hidden");
-    document.getElementById("teacherBio").setAttribute("disabled", null);
+    document.getElementById("originalTextarea").removeAttribute("hidden");
+    document.getElementById("editedTextarea").setAttribute("hidden", null);
 }
