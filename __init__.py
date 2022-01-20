7c779b803dea6bff9ace4687ad0d326e2c8e56d6
ClarenceKoh <xuxize@gmail.com>
2022-01-20 09:00:36 +0800

    App route for teacher page

--- __init__.py
+++ __init__.py
@@ -3904,128 +3904,50 @@ def contactUsManagement():
 
 """Teacher's Channel Page by Clarence"""
 
-@app.route("/teacher_page")
-@limiter.limit("30/second") # to prevent ddos attacks
-def function():
-    if "userSession" in session and "adminSession" not in session:
-        userSession = session["userSession"]
-
-        userKey, userFound, accGoodStatus, accType = validate_session_get_userKey_open_file(userSession)
-        # if there's a need to retrieve the userKey for reading the user's account details, use the function below instead of the one above
-
-        if userFound and accGoodStatus:
-            # add in your own code here for your C,R,U,D operation and remember to close() it after manipulating the data
-            userProfileImage = userKey.get_profile_image() # will return a filename, e.g. "0.png"
-            userProfileImagePath = construct_path(PROFILE_UPLOAD_PATH, userProfileImage)
-
-            # checking if the user have uploaded a profile image before and if the image file exists
-            imagesrcPath = get_user_profile_pic(userKey.get_username(), userProfileImage, userProfileImagePath)
-            return render_template('users/teacher/teacher_page.html', accType=accType, imagesrcPath=imagesrcPath)
-        else:
-            print("User not found or is banned.")
-            # if user is not found/banned for some reason, it will delete any session and redirect the user to the homepage
-            session.clear()
-            return redirect(url_for("home"))
-
-    else:
-        if "adminSession" in session:
-            return redirect(url_for("home"))
-        else:
-            # determine if it make sense to redirect the user to the home page or the login page
-            return redirect(url_for("home")) # if it make sense to redirect the user to the home page, you can delete the if else statement here and just put return redirect(url_for("home"))
-            # return redirect(url_for("userLogin"))
-
-@app.route('/teacher_page/<teacherUID>', methods=["GET","POST"]) # delete the methods if you do not think that any form will send a request to your app route/webpage
-@limiter.limit("30/second") # to prevent ddos attacks
+@app.route('/teacher_page/<teacherUID>', methods=["GET", "POST"])
+@limiter.limit("30/second")  # to prevent ddos attacks
 def teacherPage(teacherUID):
-    if "adminSession" in session:
-        adminSession = session["adminSession"]
-        print(adminSession)
-        userFound, accActive = admin_validate_session_open_file(adminSession)
-
-        if userFound and accActive:
-            return render_template('users/admin/teacher_page.html')
+    if "adminSession" in session or "userSession" in session:
+        if "adminSession" in session:
+            userSession = session["adminSession"]
         else:
-            print("Admin account is not found or is not active.")
-            # if the admin is not found/inactive for some reason, it will delete any session and redirect the user to the homepage
-            session.clear()
-            # determine if it make sense to redirect the admin to the home page or the login page or this function's html page
-            redirect(url_for("teacher_page/teacherUID"))
-            redirectURL = "/teacher_page/" + teacherUID
-            return redirect(redirectURL)
-
-    else:
-        if "userSession" in session:
             userSession = session["userSession"]
 
-            userKey, userFound, accGoodStatus, accType = validate_session_get_userKey_open_file(userSession)
+        userFound, accGoodStanding, accType, imagesrcPath = general_page_open_file(userSession)
 
-
-            if userFound and accGoodStatus:
-                # add in your code here (if any)
-                imagesrcPath = retrieve_user_profile_pic(userKey)
-                """
-                To Clarence, this template code is outdated, please use the new one for the general page
-                - Jason
-                """
-
-                return render_template('users/teacher/teacher_page.html', accType=accType, teacherUID=teacherUID, imagesrcPath=imagesrcPath)
-            else:
-                print("User not found or is banned.")
-                # if user is not found/banned for some reason, it will delete any session and redirect the user to the homepage
-                session.clear()
-                return redirect(url_for("home"))
-                # return redirect(url_for("this function name here")) # determine if it make sense to redirect the user to the home page or to this page (if you determine that it should redirect to this function again, make sure to render a guest version of the page in the else statement below)
+        if userFound and accGoodStanding:
+            return render_template('users/teacher/teacher_page.html', accType=accType, imagesrcPath=imagesrcPath, teacherUID=teacherUID)
         else:
-            # determine if it make sense to redirect the user to the home page or the login page or this function's html page
-            return render_template("users/teacher/teacher_page.html")
+            print("Admin/User account is not found or is not active/banned.")
+            session.clear()
+            return render_template("users/teacher/teacher_page.html", accType="Guest")
+    else:
+        return render_template("users/teacher/teacher_page.html", accType="Guest")
 
 """End of Teacher's Channel Page by Clarence"""
 
+
 """Teacher's Courses Page by Clarence"""
 
-@app.route('/<teacherUID>/teacher_courses', methods=["GET","POST"]) # delete the methods if you do not think that any form will send a request to your app route/webpage
+@app.route('/<teacherUID >/teacher_courses', methods=["GET","POST"])
 @limiter.limit("30/second") # to prevent ddos attacks
 def teacherCourses(teacherUID):
-    if "adminSession" in session:
-        adminSession = session["adminSession"]
-        print(adminSession)
-        userFound, accActive = admin_validate_session_open_file(adminSession)
-
-        if userFound and accActive:
-            return render_template('users/admin/teacher_courses.html')
+    if "adminSession" in session or "userSession" in session:
+        if "adminSession" in session:
+            userSession = session["adminSession"]
         else:
-            print("Admin account is not found or is not active.")
-            # if the admin is not found/inactive for some reason, it will delete any session and redirect the user to the homepage
-            session.clear()
-            # determine if it make sense to redirect the admin to the home page or the login page or this function's html page
-            return redirect("/" + teacherUID + "/teacher_courses")
-
-    else:
-        if "userSession" in session:
             userSession = session["userSession"]
 
-            userKey, userFound, accGoodStatus, accType = validate_session_get_userKey_open_file(userSession)
+        userFound, accGoodStanding, accType, imagesrcPath = general_page_open_file(userSession)
 
-
-            if userFound and accGoodStatus:
-                # add in your code here (if any)
-                imagesrcPath = retrieve_user_profile_pic(userKey)
-                """
-                To Clarence, this template code is outdated, please use the new one for the general page
-                - Jason
-                """
-
-                return render_template('users/teacher/teacher_courses.html', accType=accType, teacherUID=teacherUID, imagesrcPath=imagesrcPath)
-            else:
-                print("User not found or is banned.")
-                # if user is not found/banned for some reason, it will delete any session and redirect the user to the homepage
-                session.clear()
-                return redirect(url_for("home"))
-                # return redirect(url_for("this function name here")) # determine if it make sense to redirect the user to the home page or to this page (if you determine that it should redirect to this function again, make sure to render a guest version of the page in the else statement below)
+        if userFound and accGoodStanding:
+            return render_template('users/teacher/teacher_courses.html', accType=accType, imagesrcPath=imagesrcPath, teacherUID=teacherUID)
         else:
-            # determine if it make sense to redirect the user to the home page or the login page or this function's html page
-            return render_template("users/teacher/teacher_courses.html")
+            print("Admin/User account is not found or is not active/banned.")
+            session.clear()
+            return render_template("users/teacher/teacher_courses.html", accType="Guest")
+    else:
+        return render_template("users/teacher/teacher_courses.html", accType="Guest")
 
 """End of Teacher's Courses Page by Clarence"""
 
