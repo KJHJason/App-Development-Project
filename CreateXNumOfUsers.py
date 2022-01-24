5f27053b8f68a9e95d8d8611ea3d4351d9853d94
KJHJason <kuanjunhaojason@gmail.com>
2022-01-24 19:12:18 +0800

    Improved CreatXNumOfUsers.py

--- CreateXNumOfUsers.py
+++ CreateXNumOfUsers.py
@@ -7,15 +7,6 @@ def generate_ID(inputDict):
         generate_ID(inputDict) # using recursion if there is a collision to generate a new unique ID
     return generatedID
 
-def getLatestTestI(userDict):
-    try:
-        latestID = list(userDict.keys())[-1]
-        userObject = userDict.get(latestID)
-        username = userObject.get_username()[-1]
-        return (int(username) + 1)
-    except:
-        return 0 # meaning that there is no users in the user db yet
-
 userDict = {}
 db = shelve.open("user", "c")
 try:
@@ -30,8 +21,8 @@ except:
 
 noOfUser = int(input("How many user account to create?: "))
     
-getLatestTestI = int(getLatestTestI(userDict))
-print(getLatestTestI)
+getLatestTestI = len(userDict)
+# print(getLatestTestI)
 for i in range(getLatestTestI, noOfUser+getLatestTestI):
     hashedPwd = hash_password("123123")
     email = "test" + str(i) + "@gmail.com"
@@ -39,9 +30,8 @@ for i in range(getLatestTestI, noOfUser+getLatestTestI):
     uid = generate_ID(userDict)
     user = Student.Student(uid, username, email, hashedPwd)
     userDict[uid] = user
-    db["Users"] = userDict
-    print(f"User, {username}, created with the ID, {uid}.")
+    print(f"User {username}, created with the ID, {uid}.")
 
-
+db["Users"] = userDict
 db.close()
 print(f"{noOfUser} users created successfully.")
