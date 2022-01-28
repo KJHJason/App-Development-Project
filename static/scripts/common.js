d5204eab60f850345869570fae5e97aa547648c3
KJHJason <kuanjunhaojason@gmail.com>
2022-01-28 20:48:03 +0800

    Added go to top button for all pages

--- static/scripts/common.js
+++ static/scripts/common.js
@@ -15,3 +15,33 @@ navbtn.addEventListener("click", btnclick);
 let footerCopright = document.getElementById("copyright_year").appendChild(document.createTextNode(new Date().getFullYear()));
 
 ////////////////// End of footer copyright year Javascript //////////////////
+
+////////////////// Start of Go to top button Javascript //////////////////
+
+var scrollToTopBtn = document.getElementById("topbutton");
+
+window.onscroll = function(){
+  scrollFunction();
+};
+
+function scrollFunction(){
+  if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.classList.add("show");
+  }
+  else{
+    scrollToTopBtn.classList.remove("show");
+  }
+}
+
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+var rootElement = document.documentElement;
+
+function scrollToTop() {
+  rootElement.scrollTo({
+    top: 0, 
+    behavior: "smooth" 
+  });
+}
+
+////////////////// End of Go to top button Javascript //////////////////
